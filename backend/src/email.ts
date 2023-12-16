97c38ca2ae35482ce5332e033af8f91aee9e8bca
luc <luclammers06@gmail.com>
2023-12-16 02:23:46 +0100

    Refactor email CSS styles

--- backend/src/email.ts
+++ backend/src/email.ts
@@ -33,7 +33,6 @@ export function createEmail(body: EmailBody) {
                 flex-direction: column;
                 align-items: center;
                 justify-content: center;
-                box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
             }
     
             #content {
@@ -44,6 +43,7 @@ export function createEmail(body: EmailBody) {
                 padding: 20px;
                 border-radius: 10px;
                 text-align: center;
+                box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
             }
     
             #list {
