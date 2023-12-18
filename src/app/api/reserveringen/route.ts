d3897c516cc5852419ec3b9eb48a6c536da65c07
luc <luclammers06@gmail.com>
2023-12-18 22:55:06 +0100

    Refactor validation and authorization logic in reserveringen route

--- src/app/api/reserveringen/route.ts
+++ src/app/api/reserveringen/route.ts
@@ -12,10 +12,18 @@ export async function POST(
 
     const body = await req.json();
 
-    if (body.personen > 15 || body.personen < 1 || body.wijn > body.personen) return NextResponse.json({}, { status: 406 });
-
-    if (!body.email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/))
-        return NextResponse.json({}, { status: 406 });
+    if (
+        !body.naam ||
+        !body.email ||
+        !body.personen ||
+        !body.wijn ||
+        !body.extra ||
+        body.personen > 15 ||
+        body.personen < 1 ||
+        body.wijn > body.personen ||
+        body.wijn < 0 ||
+        !body.email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+        ) return NextResponse.json({}, { status: 406 });
 
     if (await Reservering.findOne({ email: body.email })) return NextResponse.json({}, { status: 409 });
 
@@ -55,8 +63,8 @@ export async function GET(
     req: NextRequest,
 ) {
 
-    if ((req.headers.get('Authorization') || '').split("Bearer ")[1] != process.env.BEARER_TOKEN) return NextResponse.json('', { status: 401 })
+    if ((req.headers.get('Authorization') || '').split("Bearer ")[1] != process.env.BEARER_TOKEN) return NextResponse.json({}, { status: 401 })
 
-    return NextResponse.json(await Reservering.find({}, '-_id, -__v'));
+    return NextResponse.json(await Reservering.find({}, '-_id -__v'));
 
 }
