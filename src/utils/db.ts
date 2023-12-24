6f8312ea1b0f44b28b0f493d32c02676b6cae324
luc <luclammers06@gmail.com>
2023-12-24 14:03:19 +0100

    Fix MongoDB connection in db.ts

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
-
-mongoose.connect(MONGODB_URI || '');
+mongoose.connect(process.env.MONGODB_URI || '').then(() => {
+    console.log('MongoDB connected');
+}).catch((err) => {
+    console.log(err);
+});
 
 export default mongoose;
