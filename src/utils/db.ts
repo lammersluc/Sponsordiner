d3f6b9e948a0994d90af7ec0bcb0fefd440dbea1
luc <luclammers06@gmail.com>
2023-12-25 00:29:19 +0100

    Update Dockerfile, next.config.js, postcss.config.js, and src/utils/db.ts

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-mongoose.connect(MONGODB_URI || '');
+mongoose.connect(MONGODB_URI || '')
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err: any) => console.log(err));
 
 export default mongoose;
