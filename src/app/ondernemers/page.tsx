95eb0ffd5cf5e8467bddd6f3831e665eabac27c5
luc <luclammers06@gmail.com>
2024-03-18 18:11:46 +0100

    Update sponsor dinner message and button styling

--- src/app/ondernemers/page.tsx
+++ src/app/ondernemers/page.tsx
@@ -11,9 +11,12 @@ export default function Page() {
     <div className="relative flex flex-col items-center p-14 z-10 rounded-md shadow-m">
 
         <div className="fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex items-center justify-center">
-            <button onClick={() => router.push('/')} className="bg-white p-6 rounded-md shadow-lg transition-colors text-black">
-                <p>Door omstandigheden zijn wij genoodzaakt het sponsordiner te verplaatsen naar zondag 24 maart.</p>
-                <p>Inschrijven kan door hier te klikken.</p>
+            <button onClick={() => router.push('/')} className="bg-white p-6 m-4 rounded-md shadow-lg transition-colors text-black">
+                <p>
+                  Door omstandigheden zijn wij genoodzaakt het sponsordiner te verplaatsen naar zondag 24 maart.
+                  <br/><br/>
+                  Inschrijven kan door hier te klikken.
+                </p>
             </button>
         </div>
 
